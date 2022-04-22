1f0721cbeab5a84d13358cbebba50d456461b8b8
KK-mp4 <livinglegendilmumbo@gmail.com>
2022-04-22 05:36:03 +0300

    Add project files.

---
+++ TMP Bot 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TMP_Bot_2
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = GetToken.Get();      //gets Discord bot token from gitignored file
+        }
+    }
+}
