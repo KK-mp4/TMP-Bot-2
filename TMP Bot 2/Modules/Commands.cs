51a9d3a209c6da75b18d27170fa348f111d24d10
KK-mp4 <livinglegendilmumbo@gmail.com>
2022-04-22 10:38:30 +0300

    Reading urls

--- TMP Bot 2/Modules/Commands.cs
+++ TMP Bot 2/Modules/Commands.cs
@@ -1,7 +1,10 @@
 ﻿namespace TMP_Bot_2.Modules
 {
+    using System;
     using System.Threading.Tasks;
+    using Discord;
     using Discord.Commands;
+    using Discord.WebSocket;
 
     /// <summary>
     /// General module containing commands.
@@ -23,19 +26,36 @@
         public async Task HelpAsync()
         {
             await this.Context.Channel.TriggerTypingAsync();
-            await this.ReplyAsync(
+
+
+            var embed = new EmbedBuilder()
+                .WithTitle("TMP Bot 2 Help")
+                .WithColor(new Color(255, 102, 94))
+                .AddField("Commands", "\n" +
                 "**!!tmp2 ping** Checks if bot is online\n" +
                 "**!!tmp2 help** Displays all commands\n" +
                 "**!!tmp2 add** Adds new video to the list\n" +
-                "**!!tmp2 list** Outputs full video list");
+                "**!!tmp2 list** Outputs full video list")
+                .WithFooter("Developed by KK")
+                .WithUrl("https://github.com/KK-mp4/TMP-Bot-2")
+                .Build();
+            await this.ReplyAsync(embed: embed);
         }
 
-        [Command("add")]
+        [Command("add", true)]
         [Alias("+", "new")]
         [Summary("Adds new video to the list")]
         public async Task AddAsync()
         {
-            await this.ReplyAsync("Adding...");
+            string message = this.Context.Message.Content;
+            string[] words = message.Split(' ');
+
+            await this.ReplyAsync($"Url: `{words[2]}`");
+            if (words.Length > 2)
+            {
+                await this.ReplyAsync($"Comment: `{words[3]}`");
+            }
+
         }
 
         [Command("list")]
@@ -43,7 +63,7 @@
         [Summary("Outputs full video list")]
         public async Task ListAsync()
         {
-            await this.ReplyAsync("Full videl list...");
+            await this.ReplyAsync("Full video list...");
         }
     }
 }
